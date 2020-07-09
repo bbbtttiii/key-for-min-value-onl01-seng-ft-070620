b32ca2156a06fdbcbff679a9dc6066a22a5e6736
Ben Iburg <ben.iburg@gmail.com>
2020-07-09 17:46:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,20 +11,5 @@ def key_for_min_value(hash)
       lowest_key = name
     end
   end
-  lowest_value
-end
-
-
-
-
-def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil
-  hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
-  end
   lowest_key
 end
