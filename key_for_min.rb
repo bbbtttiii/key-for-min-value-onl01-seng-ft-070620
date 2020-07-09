9eb6416860aeec72b5a133dd7f58febc0b377697
Ben Iburg <ben.iburg@gmail.com>
2020-07-09 17:04:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  fruits = {"mango": 3, "papaya": 5, "berry": 12}
-  fruits.sort
-  fruits.map do |item, num|
-    fruits[0]
+  name_hash.sort
+  name_hash.map do |name, num|
+    name_hash[0]
   end
 end
